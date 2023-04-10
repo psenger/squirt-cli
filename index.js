6ff88ef317e5cedcb551dca6009b5c5587e9888d
Philip Senger <philip@datameshgroup.com>
2023-04-10 17:27:14 +1000

    init

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
-const options = require('./lib/options')
-
-if ( options.command === 'client' ) {
-    require('./lib/client')(options)
-} else if ( options.command === 'server' ) {
-    require('./lib/server')(options)
+const options = require("./lib/options");
+const run = async () => {
+    const options = require('./lib/options')
+    if ( options.command === 'client' ) {
+        return require('./lib/client')(options)
+    } else if ( options.command === 'server' ) {
+        return require('./lib/server')(options)
+    }
 }
+run().then(console.log).catch(console.error)
