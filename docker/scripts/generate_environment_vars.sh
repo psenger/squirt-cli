40dbeb64a3cdffd439baceba0bf928d12f7855fa
psenger <philip.a.senger@cngrgroup.com>
2023-06-25 20:58:48 +1000

    ## Change
    * clean up the script
    
    ## Remove
    ## Add

--- docker/scripts/generate_environment_vars.sh
+++ docker/scripts/generate_environment_vars.sh
@@ -1,16 +1,6 @@
 #!/bin/bash -x
 PASSPHRASE=$(openssl rand -hex 32)
 SALT=$(openssl rand -hex 16)
-#echo "export PASSPHRASE=$PASSPHRASE" > /app/docker/shared/env_vars
-#echo "export SALT=$SALT" >> /app/docker/shared/env_vars
-#echo "export SERVERURL=http://server:3000/" >> /app/docker/shared/env_vars
-#echo "export PORT=3000" >> /app/docker/shared/env_vars
-#echo "export HOSTNAME=$HOSTNAME" >> /app/docker/shared/env_vars
-#echo "export DIRECTORY=/app/data" >> /app/docker/shared/env_vars
-#echo "export ENCRYPTIONALGORITHM=aes-256-cbc" >> /app/docker/shared/env_vars
-#echo "export FILE_DEPTH=3" >> /app/docker/shared/env_vars
-#echo "export DRYRUN=false" >> /app/docker/shared/env_vars
-#echo "export GLOBPATTERNS=" >> /app/docker/shared/env_vars
 {
 export PASSPHRASE=$PASSPHRASE
 export SALT=$SALT
